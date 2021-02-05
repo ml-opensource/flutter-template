5307049e830b91e792b156c774cc921e63027262
maru <maru@nodesagency.com>
2021-02-05 11:25:29 +0100

    Minor refactoring

---
+++ lib/presentation/feature/home/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/presentation/feature/home/home_page_tab.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.tab}) : super(key: key);
+
+  final HomePageTab tab;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.tab.toString()),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/presentation/feature/home/home_page_route.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_template/presentation/feature/home/home_page.dart';
+import 'package:flutter_template/presentation/feature/home/home_page_tab.dart';
+import 'package:flutter_template/presentation/app_router.dart';
+
+class HomePageRoute extends AppRoute {
+  final HomePageTab tab;
+
+  HomePageRoute({this.tab = HomePageTab.first});
+
+  @override
+  get page => HomePage(tab: tab);
+
+  @override
+  get name => '/';
+}
