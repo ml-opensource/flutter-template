b54ad7540cb75e31f6573ac757f3a68f834cd7b9
maru <maru@nodesagency.com>
2021-02-05 17:19:29 +0100

    Add light & dark mode support

--- lib/presentation/resources/colors.dart
+++ lib/presentation/resources/colors.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
 
-class _AllColors {
-  static const Color blueAccent = Colors.blueAccent;
-}
-
 class AppColors {
+  final Color background;
+  final Color surface;
   final Color accent;
+  final Color text;
 
   const AppColors({
+    @required this.background,
+    @required this.surface,
     @required this.accent,
+    @required this.text,
   });
 }
 
+const _appColorsLight = AppColors(
+  background: Colors.white,
+  surface: Colors.white,
+  accent: Colors.blueAccent,
+  text: Colors.black,
+);
+
+const _appColorsDark = AppColors(
+  background: Colors.black54,
+  surface: Colors.black12,
+  accent: Colors.lightBlueAccent,
+  text: Colors.white,
+);
+
 extension AppColorsExtension on BuildContext {
-  AppColors get colors => const AppColors(
-        accent: _AllColors.blueAccent,
-      );
+  AppColors get colors {
+    final brightness = MediaQuery.of(this).platformBrightness;
+    switch (brightness) {
+      case Brightness.light:
+        return _appColorsLight;
+      case Brightness.dark:
+        return _appColorsDark;
+      default:
+        return _appColorsLight;
+    }
+  }
 }
