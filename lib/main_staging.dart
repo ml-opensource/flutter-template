8fa7032c5adedec95fb4d790e4994619f609fd94
Jigna Patel <41483721+PatelJigna@users.noreply.github.com>
2021-02-05 19:02:11 +0530

    Set up flavors

---
+++ lib/main_staging.dart
@@ -0,0 +1,7 @@
+import 'presentation/app_flavor.dart';
+import 'main_common.dart';
+
+void main() async {
+  final config = AppFlavor.staging;
+  mainCommon(config);
+}
---
+++ lib/presentation/app_flavor.dart
@@ -0,0 +1,1 @@
+enum AppFlavor { staging, production }
