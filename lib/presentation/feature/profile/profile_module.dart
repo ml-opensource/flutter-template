76a782ba281185d6a26e44322e275c30de1eb951
Johnny Sørensen <joso@nodes.dk>
2021-07-23 14:16:57 +0200

    feat: added presenters, renamed screens/pages

---
+++ lib/presentation/feature/profile/profile_module.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter_template/injection/injector.dart';
+import 'package:flutter_template/presentation/feature/profile/profile_presenter.dart';
+
+class ProfileModule {
+	static Future<void> inject() async {
+		injector.registerFactory<ProfilePresenter>(
+				() => ProfilePresenter(),
+		);
+	}
+}
