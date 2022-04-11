8be4b050dcff23b364e004f7970cdf0d6d9effa4
Martin L. Jensen <martinloesethjensen@gmail.com>
2022-04-11 12:13:27 +0200

    fix: dependencies overriden

--- lib/data/model/auth/auth_tokens.freezed.dart
+++ lib/data/model/auth/auth_tokens.freezed.dart
@@ -1,5 +1,6 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
 
 part of 'auth_tokens.dart';
@@ -174,19 +175,21 @@ class _$_AuthTokens extends _AuthTokens {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _AuthTokens &&
-            (identical(other.accessToken, accessToken) ||
-                other.accessToken == accessToken) &&
-            (identical(other.refreshToken, refreshToken) ||
-                other.refreshToken == refreshToken) &&
-            (identical(other.tokenType, tokenType) ||
-                other.tokenType == tokenType) &&
-            (identical(other.expiresIn, expiresIn) ||
-                other.expiresIn == expiresIn));
+            const DeepCollectionEquality()
+                .equals(other.accessToken, accessToken) &&
+            const DeepCollectionEquality()
+                .equals(other.refreshToken, refreshToken) &&
+            const DeepCollectionEquality().equals(other.tokenType, tokenType) &&
+            const DeepCollectionEquality().equals(other.expiresIn, expiresIn));
   }
 
   @override
-  int get hashCode =>
-      Object.hash(runtimeType, accessToken, refreshToken, tokenType, expiresIn);
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(accessToken),
+      const DeepCollectionEquality().hash(refreshToken),
+      const DeepCollectionEquality().hash(tokenType),
+      const DeepCollectionEquality().hash(expiresIn));
 
   @JsonKey(ignore: true)
   @override
