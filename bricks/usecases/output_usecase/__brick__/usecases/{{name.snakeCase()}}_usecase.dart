c9e8a02716ab6717ec90ea5e07f03e8d5f16ae99
Martin L. Jensen <martinloesethjensen@gmail.com>
2022-04-12 09:20:58 +0200

    feat: added mason and [cubit, bloc, usecase, output_usecase]

---
+++ bricks/usecases/output_usecase/__brick__/usecases/{{name.snakeCase()}}_usecase.dart
@@ -0,0 +1,7 @@
+
+class {{name.pascalCase()}}UseCase extends OutputUseCase<{{output_type.pascalCase()}}> {
+  @override
+  {{output_type.pascalCase()}} run() {
+    // TODO: implement usecase
+  }
+}
