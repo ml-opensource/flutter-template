affd4610fe2f81cb04659e3863e2f3b1d4d06f48
Johnny Sørensen <joso@nodes.dk>
2022-05-03 15:15:19 +0200

    chore: cleanup imports

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/injection/dependencies.dart';
 import 'package:flutter_template/presentation/app.dart';
-
-import 'injection/injector.dart';
 import 'presentation/app_flavor.dart';
 
 void mainCommon(AppFlavor flavor) async {
