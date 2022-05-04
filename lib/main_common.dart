df546f2876c922476b2ef5db1a355e6f1be9891a
Johnny Sørensen <joso@nodes.dk>
2022-05-04 07:56:21 +0200

    feat: updated `android` platform (#51)
    * refactor: renamed presenter -> cubit and moved api_config to /data
    
    * feat: changed DI to `@Injectable`
    
    * feat: updated `android` platform
    
    * chore: merging leftovers
    
    * chore: merging weirdness
    
    * chore: cleanup imports
    
    * build: upgraded gradle

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/injection/dependencies.dart';
 import 'package:flutter_template/presentation/app.dart';
-
 import 'presentation/app_flavor.dart';
 
 void mainCommon(AppFlavor flavor) async {
