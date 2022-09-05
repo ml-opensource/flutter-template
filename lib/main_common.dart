b290af106f24c7b2028d8364bbaf75516f8ffaf3
Nikita Sirovskiy <nikita.sirovsky@monstar-lab.com>
2022-09-05 16:15:36 +0300

    fix: Update the code to match the new rules

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/injection/dependencies.dart';
 import 'package:flutter_template/presentation/app.dart';
+
 import 'presentation/app_flavor.dart';
 
+// ignore: avoid_void_async
 void mainCommon(AppFlavor flavor) async {
   WidgetsFlutterBinding.ensureInitialized();
   await DependencyManager.inject(flavor);
