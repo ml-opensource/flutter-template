6f849c0369920ad8bfc4d8c7e69f0ca120789c26
Nikita Sirovskiy <nikita.sirovsky@monstar-lab.com>
2023-01-03 20:52:27 +0300

    chore(#80): Remove Dio from data_module

--- lib/injection/data_module.dart
+++ lib/injection/data_module.dart
@@ -1,8 +1,3 @@
-import 'package:dio/adapter.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter_template/data/api/api_config.dart';
-import 'package:flutter_template/data/interceptor/auth_interceptor.dart';
-import 'package:flutter_template/injection/injector.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -13,24 +8,6 @@ abstract class DataModule {
   @preResolve
   Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
 
-  Dio getDio(ApiConfig apiConfig) {
-    final dio = Dio();
-    dio.httpClientAdapter = DefaultHttpClientAdapter();
-    dio.options.baseUrl = apiConfig.apiUrl;
-    dio.interceptors.add(
-      AuthInterceptor(
-        httpClient: injector(),
-        authPreferences: injector(),
-        userPreferences: injector(),
-        refreshTokenHttpClient: injector(),
-        onTokenExpired: () {
-          // TODO: Handle log out
-        },
-      ),
-    );
-
-    return dio;
-  }
   @singleton
   FlutterSecureStorage get secureStorage => const FlutterSecureStorage(
         aOptions: AndroidOptions(),
