613571254cb8c9dcb5c0a2723d411132c80fbb3d
Nikita Sirovskiy <nikita.sirovsky@monstar-lab.com>
2023-01-03 20:43:14 +0300

    chore(#80): Use ApplicationConfig

---
+++ lib/presentation/application_config.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_template/presentation/app_flavor.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'application_config.freezed.dart';
+part 'application_config.g.dart';
+
+@freezed
+class ApplicationConfig with _$ApplicationConfig {
+  factory ApplicationConfig({
+    required String apiUrl,
+    required AppFlavor flavor,
+  }) = _ApplicationConfig;
+
+  factory ApplicationConfig.fromJson(Map<String, dynamic> json) =>
+      _$ApplicationConfigFromJson(json);
+}
