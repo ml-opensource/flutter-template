c3266d8ad07db068ac67a1a929abe75858c7c0c6
AbbasMathiew <88656636+AbbasMathiew@users.noreply.github.com>
2023-08-24 03:32:21 -0500

    feat: added flavors for android and IOS (#58)
    * feat: added flavors for android and IOS
    
    * fix: specified flavor in pull request validation
    
    ---------
    
    Co-authored-by: Mathiew Abbas <mathiew95@gmail.com>
    Co-authored-by: Johnny Sørensen <joso@nodes.dk>

---
+++ lib/main_development.dart
@@ -0,0 +1,7 @@
+import 'presentation/app_flavor.dart';
+import 'main_common.dart';
+
+void main() async {
+  const config = AppFlavor.development;
+  mainCommon(config);
+}
--- lib/presentation/app_flavor.dart
+++ lib/presentation/app_flavor.dart
@@ -1,8 +1,10 @@
-enum AppFlavor { staging, production }
+enum AppFlavor { development, staging, production }
 
 extension FlavorName on AppFlavor {
   String get name {
     switch (this) {
+      case AppFlavor.development:
+        return 'development';
       case AppFlavor.staging:
         return 'staging';
       case AppFlavor.production:
